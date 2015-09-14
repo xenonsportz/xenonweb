85ba0c51623662361f04935062589ab79f92aaa3
sathish <sskmek@gmail.com>
2015-09-14 01:33:33 -0700

    init

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var IndexModel = require('../models/index');
+
+
+module.exports = function (router) {
+
+    var model = new IndexModel();
+
+    // router.get('/', function (req, res) {
+    //
+    //     res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+    //
+    // });
+
+    // Home page
+    router.get('/', function (req, res) {
+        res.render('pages/home/home', model);
+    });
+
+};
